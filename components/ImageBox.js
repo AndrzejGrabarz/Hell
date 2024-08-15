de6aebd10e48b9fd3d8952fccae79678bbc579e9
AndrzejGrabarz <andgra01@gmail.com>
2024-08-15 15:38:58 +0200

    zmiany

--- components/ImageBox.js
+++ components/ImageBox.js
@@ -12,7 +12,7 @@ function ImageBox({ lampOn, lampOff, lampa }) {
   const selectedLamp = WybierzLampe.find((e) => e.name === lampa);
   return (
     <div
-      className="flex sm:py-8 cursor-pointer  sm:my-4 "
+      className="flex flex-col sm:py-8 cursor-pointer  sm:my-4 m-10"
       onMouseOver={() => setIsLampOn(true)}
       onMouseOut={() => setIsLampOn(false)}
     >
@@ -21,7 +21,7 @@ function ImageBox({ lampOn, lampOff, lampa }) {
         query: { data: (JSON.stringify(selectedLamp)) },
       }}
       >
-        <div className="w-[200px] h-[300px]  sm:w-[400px] sm:h-[500px] md:w-[500px] md:h-[600px] lg:w-[550px] lg:h-[650px] xl:w-[250px] xl:h-[250px]">
+        <div className="w-[200px] h-[300px]  sm:w-[400px] sm:h-[500px] md:w-[500px] md:h-[600px] lg:w-[550px] lg:h-[650px] xl:w-[300px] xl:h-[300px]">
           <Image
             className="rounded shadow-2xl"
             src={`/${isLampOn ? lampOff : lampOn}.jpg`}
@@ -32,10 +32,13 @@ function ImageBox({ lampOn, lampOff, lampa }) {
             height={300}
             priority
           />
-          <div className='w-full bg-white'>Miód spadziowy</div>
-          <div className='w-full bg-white'>200 zł</div>
         </div>
       </Link>
+      <div className="flex flex-col  w-full bg-white rounded-b-lg justify-center items-center p-10 ">
+        <p>Miód spadziowy</p>
+        <p>200g</p>
+        <p>200zł</p>
+      </div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { IconContext } from 'react-icons';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import { useMemo, useState } from 'react';
@@ -28,59 +29,78 @@ export default function Home() {
     setImageIndexEnd((index) => (index + 1) % weekSpecjals.length);
   }
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col w-full mx-auto">
       <Navbar />
-      <main className="flex w-full flex-col items-center ">
-        <div className="flex">
-          <IconContext.Provider value={icon1}>
-            <IoIosArrowBack onClick={() => showPreviousImage()} />
-          </IconContext.Provider>
-          {weekSpecjals[imageIndexFirst] && (
-            <div className="specials-meal-card p-10">
-              <figure>
-                <img
-                  src={weekSpecjals[imageIndexFirst].image}
-                  alt={weekSpecjals[imageIndexFirst].alt}
-                />
-              </figure>
-            </div>
-          )}
-          {weekSpecjals[imageIndexMiddle] && (
-            <div className="specials-meal-card p-10">
-              <figure>
-                <img
-                  src={weekSpecjals[imageIndexMiddle].image}
-                  alt={weekSpecjals[imageIndexMiddle].alt}
-                />
-              </figure>
-            </div>
-          )}
-          {weekSpecjals[imageIndexEnd] && (
-            <div className="specials-meal-card p-10">
-              <figure>
-                <img
-                  src={weekSpecjals[imageIndexEnd].image}
-                  alt={weekSpecjals[imageIndexEnd].alt}
-                />
-              </figure>
-            </div>
-          )}
-          <IconContext.Provider value={icon2}>
-            <IoIosArrowForward onClick={() => showNextImage()} />
-          </IconContext.Provider>
-        </div>
-      </main>
+      <main className="flex w-full flex-col items-center " />
 
-      <div className='flex justify-center'>
-        <div className="bg-white w-[200px]">hej</div>
-          <div className="flex justify-center flex-row mt-8 sm:mt-12 flex-wrap">
-            <ImageBox lampOn="KluczOff" lampOff="KluczOn" lampa="Klucz" />
-            <ImageBox lampOn="LotosOff" lampOff="LotosOn" lampa="Lotos" />
-            <ImageBox lampOn="PasekOff" lampOff="PasekOn" lampa="Pasek" />
-            <ImageBox lampOn="KluczOff" lampOff="KluczOn" lampa="Klucz" />
-            <ImageBox lampOn="LotosOff" lampOff="LotosOn" lampa="Lotos" />
-            <ImageBox lampOn="PasekOff" lampOff="PasekOn" lampa="Pasek" />
+      <div className="max-w-[1530px] w-full mx-auto">
+        <div className=" flex justify-center align-center">
+          <div className=" max-w-[280px]text-white text-2xl font-semibold p-10">
+            Produkty
+            <ul className="text-white text-xl font-semibold">
+              <li><Link href="/" className="block py-1   text-white rounded  hover:scale-110" aria-current="page">Miody</Link></li>
+              <li><Link href="/" className="block py-1   text-white rounded  hover:scale-110" aria-current="page">Miody pitne</Link></li>
+              <li><Link href="/" className="block py-1   text-white rounded  hover:scale-110" aria-current="page">Akcesoria</Link></li>
+              <li><Link href="/" className="block py-1   text-white rounded  hover:scale-110" aria-current="page">ten tego</Link></li>
+            </ul>
           </div>
+          <div className="max-w-[1340px]flex flex-col justify center align-center">
+            <div className="ml-10">Strona główna / MIODY</div>
+            <div className="flex flex-row">
+              <ImageBox lampOn="LotosOff" lampOff="LotosOn" lampa="Klucz" />
+              <ImageBox lampOn="LotosOff" lampOff="LotosOn" lampa="Lotos" />
+              <ImageBox lampOn="PasekOff" lampOff="PasekOn" lampa="Pasek" />
+            </div>
+            <div className="flex flex-row">
+              <ImageBox lampOn="LotosOff" lampOff="LotosOn" lampa="Klucz" />
+              <ImageBox lampOn="LotosOff" lampOff="LotosOn" lampa="Lotos" />
+              <ImageBox lampOn="PasekOff" lampOff="PasekOn" lampa="Pasek" />
+            </div>
+            <div className="flex flex-row">
+              <ImageBox lampOn="LotosOff" lampOff="LotosOn" lampa="Klucz" />
+              <ImageBox lampOn="LotosOff" lampOff="LotosOn" lampa="Lotos" />
+              <ImageBox lampOn="PasekOff" lampOff="PasekOn" lampa="Pasek" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex">
+        <IconContext.Provider value={icon1}>
+          <IoIosArrowBack onClick={() => showPreviousImage()} />
+        </IconContext.Provider>
+        {weekSpecjals[imageIndexFirst] && (
+        <div className="specials-meal-card p-10">
+          <figure>
+            <img
+              src={weekSpecjals[imageIndexFirst].image}
+              alt={weekSpecjals[imageIndexFirst].alt}
+            />
+          </figure>
+        </div>
+        )}
+        {weekSpecjals[imageIndexMiddle] && (
+        <div className="specials-meal-card p-10">
+          <figure>
+            <img
+              src={weekSpecjals[imageIndexMiddle].image}
+              alt={weekSpecjals[imageIndexMiddle].alt}
+            />
+          </figure>
+        </div>
+        )}
+        {weekSpecjals[imageIndexEnd] && (
+        <div className="specials-meal-card p-10">
+          <figure>
+            <img
+              src={weekSpecjals[imageIndexEnd].image}
+              alt={weekSpecjals[imageIndexEnd].alt}
+            />
+          </figure>
+        </div>
+        )}
+        <IconContext.Provider value={icon2}>
+          <IoIosArrowForward onClick={() => showNextImage()} />
+        </IconContext.Provider>
       </div>
       <Footer />
     </div>
